0e1d936f39ca8633ce6f58ee16ea5a2a6aff00d9
Andrei <andrei.gugy@gmail.com>
2026-02-09 22:15:52 +0200

    start using react-query

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -1,73 +1,55 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
 
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
-import EventItem from './EventItem.jsx';
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { fetchEvents } from "../../utils/http.js";
 
 export default function NewEventsSection() {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+	//useQuery -> will send an http request to fetch the recently added events and display them in a list.
+	//It will also handle loading and error states.
+	const { data, isError, isPending, error } = useQuery({
+		// queryFn -> Define the actual code that will send the http request to fetch the recently added events.
+		// This function will be called by the useQuery hook when it needs to fetch the data.
+		queryFn: fetchEvents,
+		// queryKey -> A unique key that identifies this particular query.
+		// This is used by React Query to cache the data and manage the state of the query.
+		queryKey: ["events"],
+	});
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:3000/events');
+	let content;
 
-      if (!response.ok) {
-        const error = new Error('An error occurred while fetching the events');
-        error.code = response.status;
-        error.info = await response.json();
-        throw error;
-      }
+	if (isPending) {
+		content = <LoadingIndicator />;
+	}
 
-      const { events } = await response.json();
+	if (isError) {
+		content = (
+			<ErrorBlock
+				title="An error occurred"
+				message={error.cause?.message || "Failed to fetch events"}
+			/>
+		);
+	}
 
-      return events;
-    }
+	if (data) {
+		content = (
+			<ul className="events-list">
+				{data.map((event) => (
+					<li key={event.id}>
+						<EventItem event={event} />
+					</li>
+				))}
+			</ul>
+		);
+	}
 
-    fetchEvents()
-      .then((events) => {
-        setData(events);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
-
-  let content;
-
-  if (isLoading) {
-    content = <LoadingIndicator />;
-  }
-
-  if (error) {
-    content = (
-      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
-    );
-  }
-
-  if (data) {
-    content = (
-      <ul className="events-list">
-        {data.map((event) => (
-          <li key={event.id}>
-            <EventItem event={event} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  return (
-    <section className="content-section" id="new-events-section">
-      <header>
-        <h2>Recently added events</h2>
-      </header>
-      {content}
-    </section>
-  );
+	return (
+		<section className="content-section" id="new-events-section">
+			<header>
+				<h2>Recently added events</h2>
+			</header>
+			{content}
+		</section>
+	);
 }
