d603d8ee4149cd71ecae58ce87b8166a5d6a58ea
Andrei <andrei.gugy@gmail.com>
2026-02-18 12:37:20 +0200

    starting implementing post mutation for adding events and selectable img functionality

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,45 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	//POST request to add new event
+	const { mutate, isError, isLoading, error } = useMutation({
+		mutationFn: createNewEvent,
+	});
 
-  function handleSubmit(formData) {}
+	function handleSubmit(formData) {
+		mutate({ event: formData });
+	}
 
-  return (
-    <Modal onClose={() => navigate('../')}>
-      <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
-      </EventForm>
-    </Modal>
-  );
+	return (
+		<Modal onClose={() => navigate("../")}>
+			<EventForm onSubmit={handleSubmit}>
+				{isLoading && "Submitting..."}
+				{!isLoading && (
+					<>
+						<Link to="../" className="button-text">
+							Cancel
+						</Link>
+						<button type="submit" className="button">
+							Create
+						</button>
+					</>
+				)}
+			</EventForm>
+			{isError && (
+				<ErrorBlock
+					title={"Failed to create an event"}
+					message={
+						error.message?.message || "Please check the inputs and try again"
+					}
+				/>
+			)}
+		</Modal>
+	);
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -18,3 +18,40 @@ export async function fetchEvents({ signal, searchTerm }) {
 
 	return events;
 }
+
+export async function createNewEvent(eventData) {
+	const response = await fetch("http://localhost:3000/events", {
+		method: "POST",
+		body: JSON.stringify(eventData),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	const data = await response.json();
+
+	if (!response.ok) {
+		const error = new Error("An error occured while creating an event");
+		error.code = response.status;
+		error.info = data;
+		throw error;
+	}
+
+	return data.event;
+}
+
+export async function fetchSelectableImages({ params }) {
+	const response = await fetch("http://localhost:3000/events/images", {
+		params,
+	});
+	const { images } = await response.json();
+
+	if (!response.ok) {
+		const error = new Error("An error occured while fetching the images");
+		error.code = response.status;
+		error.info = images;
+
+		throw error;
+	}
+	return images;
+}
